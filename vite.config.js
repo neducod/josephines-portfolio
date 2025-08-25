1a07ff6bf61df9ef963a1be1750a27a34a1c9c61
Josephine Jerome <146160021+neducod@users.noreply.github.com>
2025-08-25 21:40:55 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,14 @@
 // // })
 // export default defineConfig({
 //   plugins: [react()],
-//   base: '/josephines-portfolio/'  // 👈 use your repo name here
+//   base: '/josephines-portfolio/'  
 // })
 
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
   plugins: [react()],
-  base: '/josephines-portfolio/', // 👈 must match your repo name exactly
+  base: '/',
 })
